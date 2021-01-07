6be5da6e68223dd817a9e3781608f1a1fe6ba757
Achraf <hardy1348@live.fr>
2021-01-07 10:12:21 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,37 @@
+// let semaine = prompt('Quel jour souhaites-tu ?')
+
+// switch (semaine) {
+//     case 'Lundi':
+//         console.log('Allez acheter des mangas et' + ' faire du sport');
+//         break;
+//     case 'Mardi':
+//         console.log('Finir JoJo');
+//         break;
+//     case 'Mercredi':
+//         console.log('Acheter des vetements');
+//         break;
+//     case 'Jeudi':
+//         console.log('Voir Bae');
+//         break;
+//     case 'Vendredi':
+//         console.log('Sortir');
+//         break;
+//     default:
+//         console.log('Y a rien');
+//         break;
+// }
+
+// Exo2
+// let age = parseInt(prompt('Ton age ?'));
+
+// switch (age > 18) {
+//     case true:
+//         console.log('Ta plus de 18 ans');
+//         break;
+//     case false:
+//         console.log('Petit gars');
+//         break;
+//     default:
+//         console.log('ya rien');
+//         break;
+// }
